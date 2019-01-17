e73728cf1570b413446cfbb96b8316f29ce2d0ed
litiansuo <2296923560@qq.com>
2019-01-17 09:20:57 +0800

    加入替换敏感字符系统

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 char arr[1000];
 int index = 0;
+int isMingan(char x)
+{
+    int count = 0;
+    for (int i = 0; i < index; i++)
+    {
+        if (x == arr[i])
+        {
+            count++;
+        }
+    }
+    if (count == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
 int main()
 {
 
@@ -21,14 +40,14 @@ int main()
         {
             printf("shuruyigeminganzifu\n");
             char m;
-            scanf("%c",&m);
-            scanf("%c",&m);
-            arr[index]=m;
+            scanf("%c", &m);
+            scanf("%c", &m);
+            arr[index] = m;
             index++;
             printf("tianjiachenggong,dianjihuichejixu\n");
             char x;
-            scanf("%c",&x);
-            scanf("%c",&x);
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 2)
         {
@@ -36,22 +55,41 @@ int main()
             index--;
             printf("shanchuchenggong,dianjihuichejixu\n");
             char x;
-            scanf("%c",&x);
-            scanf("%c",&x);
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 3)
         {
             printf("xianshisuoyouminganzifu\n");
-            for(int i=0;i<index;i++){
-                printf("%c\n",arr[i]);
+            for (int i = 0; i < index; i++)
+            {
+                printf("%c\n", arr[i]);
             }
             printf("dianjihuichejixu\n");
             char x;
-            scanf("%c",&x);
-            scanf("%c",&x);
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 4)
         {
+            printf("qingshuruyiduanyingwen\n");
+            char neirong[1000];
+            scanf("%s",neirong);
+            for(int i = 0;1;i++){
+                if(neirong[i] == '\0'){
+                    break;
+                }
+                if(isMingan(arr[i]) == 1){
+                    neirong[i] = '*';
+                }
+                printf("%c",neirong[i]);
+            }
+            printf("\n");
+
+            printf("tihuanwancheng,dianjihuichejixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
         }
         if (code == 5)
         {
