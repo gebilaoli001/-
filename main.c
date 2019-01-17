15f1fe8f661fbd7ef695be74c6cd01c37c528d07
litiansuo <2296923560@qq.com>
2019-01-17 08:50:31 +0800

    创建工程基本框架

--- main.c
+++ main.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
+char arr[1000];
+int index = 0;
+int main()
+{
 
-int main(){
+    printf("jinruminganzifuguanlixitong\n");
 
-    printf("hello world!");
+    while (1)
+    {
+        printf("1---tianjiaminganzifu\n");
+        printf("2---shanchuminganzifu\n");
+        printf("3---chakansuoyouminganzifu\n");
+        printf("4---tihuannierongzhongdeminganzifu\n");
+        printf("5---tuichu\n");
+
+        printf("qingshurugongnengbianhao\n");
+        int code;
+        scanf("%d", &code);
+        if (code == 1)
+        {
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+            printf("dianjihuichetuichu\n");
+            break;
+        }
+    }
 
     return 0;
-
 }
